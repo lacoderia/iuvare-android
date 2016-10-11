c77f80b95de8a8e5302d145e534334417254c0f1
tonklis <benjamin.hernandez@in2teck.com>
2016-10-11 17:05:18 -0500

    Initial commit

---
+++ app/src/main/java/mx/coderia/iuvareapp/MainActivity.java
@@ -0,0 +1,38 @@
+package mx.coderia.iuvareapp;
+
+import android.content.pm.ApplicationInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        mWebView.setWebChromeClient(new WebChromeClient());
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.loadUrl("http://socios.iuvare.mx");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
