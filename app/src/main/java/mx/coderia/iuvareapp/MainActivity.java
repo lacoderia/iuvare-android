89bc070a8ea39809077647ced71b006f992a8a21
schultzRr <ZFja5Hdb>
2016-10-13 18:18:45 -0500

    Se agregó una alerta en caso de que el usuario no tenga conexión a internet

--- app/src/main/java/mx/coderia/iuvareapp/MainActivity.java
+++ app/src/main/java/mx/coderia/iuvareapp/MainActivity.java
@@ -1,12 +1,19 @@
 package mx.coderia.iuvareapp;
 
-import android.content.pm.ApplicationInfo;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,16 +22,79 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        mWebView = (WebView) findViewById(R.id.activity_main_webview);
-        mWebView.setWebChromeClient(new WebChromeClient());
+        loadWebView();
+    }
 
-        // Enable Javascript
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+    public void loadWebView() {
 
-        mWebView.loadUrl("http://socios.iuvare.mx");
+        if (isOnline()){
+            setContentView(R.layout.activity_main);
+
+            mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+            mWebView.setWebViewClient(new WebViewClient() {
+                @Override
+                public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+                    Log.println(Log.DEBUG, "ERROR", "ERROR GENERICO");
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                    builder
+                            .setTitle("Lo sentimos")
+                            .setMessage("Ocurrió un error al cargar el contenido de la app.")
+                            .setPositiveButton(R.string.retry, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int id) {
+                                    loadWebView();
+                                }
+                            });
+
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    Log.println(Log.DEBUG, "SUCCESS", "SUCCESS GENERICO");
+                }
+            });
+
+            mWebView.setWebChromeClient(new WebChromeClient());
+
+            // Enable Javascript
+            WebSettings webSettings = mWebView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+
+            mWebView.loadUrl("http://socios.iuvare.mxkjsd");
+
+        } else {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+            builder
+                    .setTitle("No hay conexión a internet")
+                    .setMessage("Conéctate a una red Wi-Fi o habilita tus datos celulares.")
+                    .setPositiveButton(R.string.retry, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            loadWebView();
+                        }
+                    });
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnected()) {
+            return true;
+        }
+        return false;
     }
 
     @Override
